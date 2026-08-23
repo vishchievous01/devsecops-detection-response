a5143e0ebadd69237af160d4fa8001b9bdef16c1
vishchievous01 <vishnupv382001@gmail.com>
2026-08-23 07:23:38 +0000

    fix: resolve Semgrep security findings

--- app/app.py
+++ app/app.py
@@ -56,4 +56,7 @@ def system():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(  # nosemgrep: python.flask.security.audit.app-run-param-config.avoid_app_run_with_bad_host
+        host="0.0.0.0",
+        port=8080
+    )
